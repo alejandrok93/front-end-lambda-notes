138c3116a6ddd5556e66f210a814a31e4d4a5367
alejandrok93 <alejandrok93@gmail.com>
2019-01-30 16:05:59 -0800

    Updated Authenticate HOC to fix if jwt is set to undefined

--- src/components/Authenticate/Authenticate.js
+++ src/components/Authenticate/Authenticate.js
@@ -1,24 +1,29 @@
-import React from "react";
-import LoginForm from "../LoginForm/LoginForm.js";
+import React from 'react';
+import LoginForm from '../LoginForm/LoginForm.js';
 
 const authenticate = HOCApp =>
-  class extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { loggedIn: false };
-    }
+	class extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = { loggedIn: false };
+		}
 
-    componentDidMount() {
-      //check local storage to see if JWT token exists
-      if (localStorage.getItem("jwt")) {
-        this.setState({ loggedIn: true });
-      }
-    }
+		componentDidMount() {
+			//check local storage to see if JWT token exists
+			if (
+				localStorage.getItem('jwt') &&
+				localStorage.getItem('jwt') != 'undefined'
+			) {
+				console.log('hi friend');
+				console.log(localStorage.getItem('jwt'));
+				this.setState({ loggedIn: true });
+			}
+		}
 
-    render() {
-      console.log(this.state.loggedIn);
-      return this.state.loggedIn ? <HOCApp /> : <LoginForm />;
-    }
-  };
+		render() {
+			console.log(this.state.loggedIn);
+			return this.state.loggedIn ? <HOCApp /> : <LoginForm />;
+		}
+	};
 
 export default authenticate;
